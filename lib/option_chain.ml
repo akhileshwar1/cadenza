cf12ae7201177414dafe756feed27187a76ff933
Akhil Kandi <akhilprm999@gmail.com>
2025-05-07 09:33:13 +0400

    Comment option_chain logs.

--- lib/option_chain.ml
+++ lib/option_chain.ml
@@ -16,22 +16,22 @@ let log msg = Printf.printf "[LOG] %s\n%!" msg
 let safe_to_string key json =
   match json |> member key with
   | `String s -> s
-  | `Null -> log (Printf.sprintf "Key '%s' is null" key); ""
-  | _ -> log (Printf.sprintf "Key '%s' is not a string" key); ""
+  | `Null -> (* log (Printf.sprintf "Key '%s' is null" key); *) ""
+  | _ -> (* log (Printf.sprintf "Key '%s' is not a string" key); *) ""
 
 let safe_to_float key json =
   match json |> member key with
   | `Float f -> f
   | `Int i -> float_of_int i
-  | `Null -> log (Printf.sprintf "Key '%s' is null" key); nan
-  | _ -> log (Printf.sprintf "Key '%s' is not a float" key); nan
+  | `Null -> (* log (Printf.sprintf "Key '%s' is null" key); *) nan
+  | _ -> (* log (Printf.sprintf "Key '%s' is not a float" key); *) nan
 
 let parse_option_data json : option_data =
   let symbol = safe_to_string "symbol" json in
   let ltp = safe_to_float "ltp" json in
   let delta = safe_to_float "delta" json in
   let strike = safe_to_string "strike" json in
-  log (Printf.sprintf "Parsed option: %s ltp=%f delta=%f %s" symbol ltp delta strike);
+  (* log (Printf.sprintf "Parsed option: %s ltp=%f delta=%f %s" symbol ltp delta strike); *)
   {
     symbol;
     ltp;
@@ -40,27 +40,27 @@ let parse_option_data json : option_data =
   }
 
 let parse (json : Yojson.Safe.t) : t =
-  log "Starting parse";
+  (* log "Starting parse"; *)
   json
   |> to_assoc
   |> List.map (fun (expiry, strikes_json) ->
-    log (Printf.sprintf "Parsing expiry: %s" expiry);
+    (* log (Printf.sprintf "Parsing expiry: %s" expiry); *)
     let strikes =
       strikes_json
       |> to_assoc
       |> List.map (fun (strike_str, contracts_json) ->
-        log (Printf.sprintf "  Strike: %s" strike_str);
+        (* log (Printf.sprintf "  Strike: %s" strike_str); *)
         let strike =
           try float_of_string strike_str
           with Failure _ ->
-            log (Printf.sprintf "  Failed to convert strike '%s' to float" strike_str);
+            (* log (Printf.sprintf "  Failed to convert strike '%s' to float" strike_str); *)
             nan
         in
         let contracts =
           contracts_json
           |> to_assoc
           |> List.map (fun (sym, data) ->
-            log (Printf.sprintf "    Contract: %s" sym);
+            (* log (Printf.sprintf "    Contract: %s" sym); *)
             (sym, parse_option_data data))
         in
         (strike, contracts)
